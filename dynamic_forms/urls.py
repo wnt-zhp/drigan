f4e004fe96db109e894e4d6876319117442f1643
patchfly <patchfly@gmail.com>
2013-11-02 21:21:06 +0100

    registration form

--- dynamic_forms/urls.py
+++ dynamic_forms/urls.py
@@ -3,6 +3,7 @@ from dynamic_forms import views
 
 urlpatterns = patterns('',
     (r'^add/(?P<dynamic_form_id>\d+)/$', views.add_dynamic_form_field),
+    (r'^fill/(?P<dynamic_form_id>\d+)/$', views.fill_form),
     (r'^add/(?P<content_type_model>[-\w]+)/(?P<object_id>\d+)/$',
      views.add_dynamic_form),
 )
--- dynamic_forms/views.py
+++ dynamic_forms/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from dynamic_forms.forms import AddDynamicFormField, BaseDynamicForm
-from dynamic_forms.models import DynamicForm
+from dynamic_forms.models import DynamicForm, DynamicFormData
 from django.template import RequestContext
 from django import http
 from django.core.urlresolvers import reverse
@@ -42,4 +42,25 @@ def add_dynamic_form_field(request, dynamic_form_id):
                               {"form": form,
                                "dynamic_form": dynamic_form,
                                "dynamic_form_form": dynamic_form_form},
+                              context_instance=RequestContext(request))
+
+
+@login_required
+def fill_form(request, dynamic_form_id):
+    dynamic_form = get_object_or_404(DynamicForm, pk=dynamic_form_id)
+    if request.method == 'POST':
+        form = BaseDynamicForm(dynamic_form, request.POST)
+        if form.is_valid():
+            filled_data = form.cleaned_data
+            DynamicFormData.objects.create(form=dynamic_form,
+                                           user=request.user,
+                                           data=filled_data)
+            messages.success(request,
+                             _('Form has been filled successfully.'))
+            return http.HttpResponseRedirect("/")
+    else:
+        form = BaseDynamicForm(dynamic_form)
+    return render_to_response("dynamic_forms/form_fill.html",
+                              {"dynamic_form": dynamic_form,
+                               "dynamic_form_form": form},
                               context_instance=RequestContext(request))
