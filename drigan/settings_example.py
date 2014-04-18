9aadb096f72393e3f57e0b7783649ad7a5621c37
Jacek Bzdak <jbzdak@gmail.com>
2014-04-18 21:40:25 +0200

    Well

--- drigan/settings_example.py
+++ drigan/settings_example.py
@@ -3,7 +3,6 @@ from .settings_shared import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -42,13 +41,13 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/tmp/drigan-static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
